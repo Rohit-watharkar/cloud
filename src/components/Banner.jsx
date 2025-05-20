abed6cdbdb49cfb7c9ccb38c887e4e9bc4008ab1
Rohit Watharkar <121039406+Rohit-watharkar@users.noreply.github.com>
2025-05-20 11:08:57 +0530

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -11,6 +11,7 @@ function Banner() {
               <span className="text-pink-500">new everyday!!!</span>
             </h1>
             <p className="text-sm md:text-xl">
+              i have add some lines in my project
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor,
               et totam. Tempora amet atque expedita, quae corrupti totam sed
               pariatur corporis at veniam est voluptas animi!
